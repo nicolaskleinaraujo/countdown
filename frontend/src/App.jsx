09fe3cd074bb01a126eacf3ed88ff9ecf88cefff
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-09-19 21:40:59 -0300

    creates the router file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 // CSS
 import './App.css'
 
+// Modules
+import Router from './utils/router'
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <Router />
     </>
   )
 }
---
+++ frontend/src/utils/Router.jsx
@@ -0,0 +1,16 @@
+// Modules
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+const Router = () => {
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" />
+                </Routes>
+            </BrowserRouter>
+        </>
+    )
+}
+
+export default Router
