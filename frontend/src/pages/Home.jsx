53d3ca4913119dfadfb72aaca8675171767541a0
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-09-19 21:47:30 -0300

    add cart component to the project

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,16 @@
+// Modules
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+
 const Home = () => {
     return (
-        <div>Home</div>
+        <div>
+            <h1>NOSSO temporizador de eventos importantes!</h1>
+
+            <Avatar>
+                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarFallback>Foto Casal</AvatarFallback>
+            </Avatar>
+        </div>
     )
 }
 
