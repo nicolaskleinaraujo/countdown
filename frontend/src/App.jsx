dddccb2ca6ae60f38a6d7532fda977b3e60f5ceb
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-09-19 21:32:33 -0300

    hotfixes the tailwindcss configs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,9 @@ import './App.css'
 function App() {
   return (
     <>
-      <p>Hello World</p>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </>
   )
 }
