05f58bbef24a2824eb41961b72136e9142aa1e51
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-10-16 13:21:56 -0300

    adds the deleteUser to the users router

--- backend/src/controllers/user/index.ts
+++ backend/src/controllers/user/index.ts
@@ -1,6 +1,8 @@
 // Controllers
 import { createUser } from "./createUser"
+import { deleteUser } from "./deleteUser"
 
 export const userController = {
     createUser,
+    deleteUser,
 }
--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -4,6 +4,7 @@ import { userController } from "../controllers/user"
 const usersRouter: IRouter = express.Router()
 
 // Routes
-usersRouter.route("/").post((req:Request, res:Response) => userController.createUser(req, res))
+usersRouter.route("/").post((req: Request, res: Response) => userController.createUser(req, res))
+usersRouter.route("/").delete((req: Request, res: Response) => userController.deleteUser(req, res))
 
 export default usersRouter
