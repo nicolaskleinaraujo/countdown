da32aa1ead75f9b04a582e2ea3fa44767a6cbf63
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-10-16 16:39:08 -0300

    hotfixes the validateToken behavior

--- backend/src/middleware/validateToken.ts
+++ backend/src/middleware/validateToken.ts
@@ -1,16 +1,41 @@
 import { Request, Response, NextFunction } from "express"
 import jwt, { JwtPayload } from "jsonwebtoken"
+import { prisma } from "../db/client"
 
 interface jwtInfos extends JwtPayload {
     id: string,
 }
 
-export const validateToken = async(req: Request, res: Response, next: NextFunction) => {
-    const userId: string | string[] | undefined = req.headers.userId
+export const validateToken = async(req: Request, res: Response, next: NextFunction): Promise<void> => {
+    const userId: string | string[] | undefined = req.headers.userid
 
     if (userId === undefined) {
-        res.clearCookie
         res.status(401).json({ msg: "Informações insuficientes" })
+        return
+    }
+
+    const user = await prisma.user.findUnique({ where: { id: Number(userId) } })
+    if (!user) {
+        res.status(400).json({ msg: "Sessão expirada, faça o login novamente" })
+        return
+    }
+
+    // Checking 
+    const refreshToken = req.signedCookies.refresh
+    if (!refreshToken) {
+        res.status(401).json({ msg: "Sessão expirada, faça o login novamente" })
+        return
+    }
+
+    try {
+        const refreshJwt = jwt.verify(refreshToken, process.env.JWT_SECRET as string) as jwtInfos
+
+        if (refreshJwt.id != userId) {
+            res.status(401).json({ msg: "Sessão expirada, faça o login novamente" })
+            return
+        }
+    } catch (error) {
+        res.status(401).json({ msg: "Sessão expirada, faça o login novamente" })
         return
     }
 
@@ -23,39 +48,21 @@ export const validateToken = async(req: Request, res: Response, next: NextFuncti
     try {
         const accessJwt = jwt.verify(accessToken, process.env.JWT_SECRET as string) as jwtInfos
 
-        if (accessJwt.id !== userId) {
+        if (accessJwt.id != userId) {
             res.status(401).json({ msg: "Sessão expirada, faça o login novamente" })
             return
         }
-    } catch { return }
+    } catch {
+        const newAccessToken: string = jwt.sign({ id: userId }, process.env.JWT_SECRET as string, { expiresIn: "1h" })
 
-    const refreshToken = req.signedCookies.refresh
-    if (!refreshToken) {
-        res.status(401).json({ msg: "Sessão expirada, faça o login novamente" })
-        return
+        res.cookie("access", newAccessToken, {
+            httpOnly: true,
+            signed: true,
+            secure: true,
+            sameSite: "none",
+            maxAge: 1 * 60 * 60 * 1000,
+        })
     }
-
-    try {
-        const refreshJwt = jwt.verify(refreshToken, process.env.JWT_SECRET as string) as jwtInfos
-
-        if (refreshJwt.id !== userId) {
-            res.status(401).json({ msg: "Sessão expirada, faça o login novamente" })
-            return
-        }
-    } catch (error) {
-        res.status(401).json({ msg: "Sessão expirada, faça o login novamente" })
-        return
-    }
-
-    const newAccessToken = jwt.sign({ id: userId }, process.env.JWT_SECRET as string, { expiresIn: "1h" })
-
-    res.cookie("access", newAccessToken, {
-        httpOnly: true,
-        signed: true,
-        secure: true,
-        sameSite: "none",
-        maxAge: 1 * 60 * 60 * 1000,
-    })
 
     next()
 }
--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,11 +1,13 @@
 // Modules
 import express, { Request, Response, IRouter } from "express"
 import { userController } from "../controllers/user"
+import { validateToken } from "../middleware/ValidateToken"
+
 const usersRouter: IRouter = express.Router()
 
 // Routes
 usersRouter.route("/").post((req: Request, res: Response) => userController.createUser(req, res))
-usersRouter.route("/").delete((req: Request, res: Response) => userController.deleteUser(req, res))
+usersRouter.route("/").delete(validateToken, (req: Request, res: Response) => userController.deleteUser(req, res))
 usersRouter.route("/login").post((req: Request, res: Response) => userController.createLogin(req, res))
 
 export default usersRouter
