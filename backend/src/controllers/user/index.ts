457e50b11c234577502b9feb379ad48b7aae2427
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-10-16 14:42:52 -0300

    adds the createLogin to the users router

--- backend/src/controllers/user/index.ts
+++ backend/src/controllers/user/index.ts
@@ -1,8 +1,10 @@
 // Controllers
+import { createLogin } from "./createLogin"
 import { createUser } from "./createUser"
 import { deleteUser } from "./deleteUser"
 
 export const userController = {
     createUser,
     deleteUser,
+    createLogin,
 }
--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -6,5 +6,6 @@ const usersRouter: IRouter = express.Router()
 // Routes
 usersRouter.route("/").post((req: Request, res: Response) => userController.createUser(req, res))
 usersRouter.route("/").delete((req: Request, res: Response) => userController.deleteUser(req, res))
+usersRouter.route("/login").post((req: Request, res: Response) => userController.createLogin(req, res))
 
 export default usersRouter
