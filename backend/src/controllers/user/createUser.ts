9bf12a7e305b1c6c6cb13cd23f503b3e9fc9e1fe
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-10-16 12:08:49 -0300

    adds email validation on createUser

--- backend/src/controllers/user/createUser.ts
+++ backend/src/controllers/user/createUser.ts
@@ -2,21 +2,35 @@ import { prisma } from "../../db/client"
 import { Request, Response } from "express"
 import { z } from "zod"
 
+interface User {
+    email: string,
+    password: string,
+}
+
 const UserSchema = z.object({
     email: z.string().email().max(150).trim(),
     password: z.string().max(150).trim().min(5),
 })
 
-export const createUser = async(req: Request, res: Response) => {
+export const createUser = async(req: Request, res: Response): Promise<void> => {
     try {
-        const data = UserSchema.parse(req.body)
+        const { email, password }: User = UserSchema.parse(req.body)
 
-        res.status(200).json({ data, msg: "Usuario criado com sucesso" })
+        const emailExists = await prisma.user.findUnique({ where: { email } })
+        if (emailExists) {
+            res.status(400).json({ msg: "Email já cadastrado" })
+            return
+        }
+
+        res.status(200).json({ msg: "Usuario criado com sucesso" })
     } catch (error) {
         if (error instanceof z.ZodError) {
             const fields = error.issues.flatMap(issue => issue.path.map(path => path))
             res.status(400).json(fields)
+            return
         }
+
+        res.status(500).json({ msg: "Erro interno, tente novamente" })
     }
 
 }
