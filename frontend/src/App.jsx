66999badd6a779266545c8c4f82816288074292f
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-10-17 10:27:38 -0300

    creates the tryAuth on app file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,23 @@
-// CSS
-import './App.css'
-
 // Modules
 import Router from './utils/router'
+import dbFetch from './config/axios'
+import { useContext, useEffect } from 'react'
+import { UserContext } from './context/UserContext'
 
 function App() {
+  const { setUserId, setUserPages } = useContext(UserContext)
+
+  const tryAuth = async() => {
+    const res = await dbFetch.post("/users/tryauth")
+
+    setUserId(res.data.user.id)
+    setUserPages(res.data.user.pages)
+  }
+
+  useEffect(() => {
+    tryAuth()
+  }, [])
+
   return (
     <>
       <Router />
