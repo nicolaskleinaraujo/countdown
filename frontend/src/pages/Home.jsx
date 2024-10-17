6bafadc683430900e9f7a1324842d39b5d156e1b
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-10-17 11:54:35 -0300

    finalizes the home page Pages rendering

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,18 @@
+// Components
+import { Card, CardHeader, CardTitle } from "@/components/ui/card"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+
 // Modules
 import { useContext } from "react"
 import { UserContext } from "@/context/UserContext"
 import { Link } from "react-router-dom"
+import { Button } from "@/components/ui/button"
 
 const Home = () => {
     const { userPages } = useContext(UserContext)
 
     return (
-        <div>
+        <div className="flex flex-col bg-bgcolor h-screen pt-5">
             { userPages.length === 0 ? (
                 <>
                     <p>Crie uma "Page" personalizada</p>
@@ -16,12 +21,22 @@ const Home = () => {
                 <>
                     { userPages.map(page => (
                         <Link to={`/pages/${page.id}`} key={page.id}>
-                            <img src={`data:image/png;base64,${page.image.content}`} alt="Imagem da Page" />
-                            <p>{page.title}</p>
+                            <Card className="text-cent mx-5 mb-5 p-2 text-center">
+                                <Avatar className="mx-auto w-28 h-28 border-2 border-bgcolor">
+                                    <AvatarImage src={`data:image/png;base64,${page.image.content}`} />
+                                    <AvatarFallback>Imagem da Page</AvatarFallback>
+                                </Avatar>
+
+                                <CardHeader>
+                                    <CardTitle>{page.title}</CardTitle>
+                                </CardHeader>
+                            </Card>
                         </Link>
                     ))}
 
-                    <p>Nova "Page"</p>
+                    <div className="flex justify-center items-center">
+                        <Button>Nova Page</Button>
+                    </div>
                 </>
             )}
         </div>
