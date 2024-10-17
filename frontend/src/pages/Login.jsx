efa378cfc8b60c5dc24ad9477776cbfb5de4bfde
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-10-17 09:33:46 -0300

    creates the handleLogin function

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -7,9 +7,11 @@ import { Button } from "@/components/ui/button"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
+import { Link } from "react-router-dom"
+import dbFetch from "@/config/axios"
 
 const formSchema = z.object({
-    email: z.string().email({ message: "Digite um email válido" }).trim().max(150),
+    email: z.string().email({ message: "Digite um email válido" }).trim().min(3, { message: "Minimo de 3 caracteres" }).max(150),
     password: z.string().trim().min(5, { message: "Minimo de 5 caracteres" }).max(150),
 })
 
@@ -22,23 +24,34 @@ const Login = () => {
         }
     })
 
-    const handleLogin = (values) => {
-        console.log(values)
+    const handleLogin = async(values) => {
+        try {
+            const res = await dbFetch.post("/users/login", {
+                email: values.email,
+                password: values.password,
+            })
+
+            console.log(res.data)
+        } catch (error) {
+            console.log(error)
+        }
     }
 
     return (
-        <div>
+        <div className="flex justify-center items-center flex-col bg-bgcolor h-screen">
+            <h2 className="text-textcolor text-2xl mb-5">Faça o seu login</h2>
             <Form {...form}>
                 <form onSubmit={form.handleSubmit(handleLogin)}>
                     <FormField
                         control={form.control}
                         name="email"
                         render={({ field }) => (
-                            <FormItem>
+                            <FormItem className="w-60 mb-5 text-textcolor">
                                 <FormLabel>Email</FormLabel>
                                 <FormControl>
                                     <Input placeholder="Digite seu email" {...field} />
                                 </FormControl>
+                                <FormMessage />
                             </FormItem>
                         )}
                     />
@@ -47,7 +60,7 @@ const Login = () => {
                         control={form.control}
                         name="password"
                         render={({ field }) => (
-                            <FormItem>
+                            <FormItem className="text-textcolor">
                                 <FormLabel>Senha</FormLabel>
                                 <FormControl>
                                     <Input placeholder="Digite sua senha" {...field} />
@@ -57,9 +70,10 @@ const Login = () => {
                         )}
                     />
 
-                    <Button type="submit">Login</Button>
+                    <div className="flex justify-center mt-5"><Button type="submit">Login</Button></div>
                 </form>
             </Form>
+            <p className="text-textcolor mt-5">Não tem uma conta? <Button className="text-textcolor p-0" variant="link"><Link to={"/register"}>Criar</Link></Button></p>
         </div>
     )
 }
