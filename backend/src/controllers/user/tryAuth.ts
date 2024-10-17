b550f354d3afc4416e661b1aae2b782799beda98
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-10-17 11:25:35 -0300

    adds base64 image return on tryAuth

--- backend/src/controllers/user/tryAuth.ts
+++ backend/src/controllers/user/tryAuth.ts
@@ -1,14 +1,28 @@
 import { Request, Response, NextFunction } from "express"
 import jwt, { JwtPayload } from "jsonwebtoken"
 import { prisma } from "../../db/client"
-import { User } from "@prisma/client"
+import { User, Pages, Images } from "@prisma/client"
 
 interface jwtInfos extends JwtPayload {
     id: number,
 }
 
+interface ImagesInfos {
+    id: number
+    filename: string
+    content: string | Buffer
+}
+
+interface PagesInfos extends Pages {
+    image: ImagesInfos,
+}
+
+interface UserInfos extends User {
+    pages: PagesInfos[],
+}
+
 export const tryAuth = async(req: Request, res: Response): Promise<void> => {
-    let user: User | null
+    let user: UserInfos | null = null
 
     // Validating the refresh token
     const refreshToken = req.signedCookies.refresh
@@ -20,7 +34,15 @@ export const tryAuth = async(req: Request, res: Response): Promise<void> => {
     try {
         const refreshJwt = jwt.verify(refreshToken, process.env.JWT_SECRET as string) as jwtInfos
 
-        user = await prisma.user.findUnique({ where: { id: refreshJwt.id }, include: { pages: { include: { image: true } } } })
+        user = await prisma.user.findUnique({ 
+            where: { id: refreshJwt.id },
+            include: {
+                pages: {
+                    include: { image: true } 
+                },
+            },
+        })
+
         if (user === null) {
             res.status(400).json({ msg: "Sessão expirada, faça o login novamente" })
             return
@@ -60,6 +82,9 @@ export const tryAuth = async(req: Request, res: Response): Promise<void> => {
             maxAge: 1 * 60 * 60 * 1000,
         })
     }
+
+    // Convert pages images to base64
+    user.pages.forEach(page => page.image.content = page.image.content.toString("base64"))
 
     // Removes password before sending to client
     user.password = ""
