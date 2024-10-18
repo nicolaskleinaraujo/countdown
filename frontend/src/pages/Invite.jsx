12d900b74bd8c7daab854978b4705082535b056f
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-10-18 17:17:36 -0300

    creates the handleInvite function

--- frontend/src/pages/Invite.jsx
+++ frontend/src/pages/Invite.jsx
@@ -1,12 +1,45 @@
 // Components
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+import { ReloadIcon } from "@radix-ui/react-icons"
 
 // Modules
-import { useParams } from "react-router-dom"
+import { useContext, useState } from "react"
+import { useParams, Link, useNavigate } from "react-router-dom"
+import { toast } from "react-toastify"
+import { UserContext } from "@/context/UserContext"
+import { RedirectContext } from "@/context/RedirectContext"
+import dbFetch from "@/config/axios"
 
 const Invite = () => {
     const { token } = useParams()
+    const [loading, setLoading] = useState(false)
+    const navigate = useNavigate()
+
+    const { userId, userPages, setUserPages } = useContext(UserContext)
+    const { setRedirect } = useContext(RedirectContext)
+
+    const handleInvite = async() => {
+        if (userId === 0) {
+            setRedirect(`/invite/${token}`)
+            navigate("/login")
+        }
+
+        setLoading(true)
+
+        try {
+            const res = await dbFetch.get(`/pages/invite/${token}`, {
+                headers: { "userId": userId }
+            })
+
+            setUserPages([...userPages, res.data.page])
+
+            navigate(`/pages/${res.data.page.id}`)
+        } catch (error) {
+            toast.error(error.response.data.msg)
+            navigate("/")
+        }
+    }
 
     return (
         <div className="flex flex-col justify-center items-center bg-bgcolor w-screen min-h-screen">
@@ -20,8 +53,17 @@ const Invite = () => {
                 </CardContent>
 
                 <CardFooter className="flex flex-row justify-center items-center">
-                    <Button className="mr-5">Aceitar</Button>
-                    <Button variant="destructive">Recusar</Button>
+                    {!loading ? (
+                        <>
+                            <Button onClick={handleInvite} className="mr-5">Aceitar</Button>
+
+                            <Button variant="destructive" asChild>
+                                <Link to={"/"}>Recusar</Link>
+                            </Button>
+                        </>
+                    ) : (
+                        <Button disabled><ReloadIcon className="mr-2 h-4 w-4 animate-spin" />Carregando</Button>
+                    )}
                 </CardFooter>
             </Card>
         </div>
