a2f1fb24da72bc55c8c64c674a8b9296850a28ac
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-10-18 15:14:54 -0300

    adds image base64 convert to updatePageImage

--- backend/src/controllers/pages/updatePageImage.ts
+++ backend/src/controllers/pages/updatePageImage.ts
@@ -2,6 +2,17 @@ import { prisma } from "../../db/client"
 import { Request, Response } from "express"
 import { z } from "zod"
 import sharp = require("sharp")
+import { Pages } from "@prisma/client"
+
+interface ImagesInfos {
+    id: number
+    filename: string
+    content: string | Buffer
+}
+
+interface PagesInfos extends Pages {
+    image: ImagesInfos,
+}
 
 interface Infos {
     image: Express.Multer.File,
@@ -20,7 +31,7 @@ export const updatePageImage = async(req: Request, res: Response): Promise<void>
             id: req.body.id
         })
 
-        const updatedPage = await prisma.pages.update({
+        const updatedPage: PagesInfos = await prisma.pages.update({
             where: { id: Number(id) },
             data: {
                 image: {
@@ -32,6 +43,8 @@ export const updatePageImage = async(req: Request, res: Response): Promise<void>
             },
             include: { Events: true, image: true },
         })
+
+        updatedPage.image.content = updatedPage.image.content.toString("base64")
 
         res.status(200).json({ msg: "Pagina atualizada com sucesso", updatedPage })
     } catch (error) {
