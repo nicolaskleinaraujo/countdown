4e8fb1abe2397368fb65e1563bd72a9ba52140eb
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-10-19 17:10:36 -0300

    adds setRedirect to userRoute

--- frontend/src/utils/UserRoute.jsx
+++ frontend/src/utils/UserRoute.jsx
@@ -2,10 +2,14 @@
 import { Outlet, Navigate, useLocation } from "react-router-dom"
 import { useContext } from "react"
 import { UserContext } from "../context/UserContext"
+import { RedirectContext } from "@/context/RedirectContext"
 import useAuth from "../hooks/useAuth"
 
 const UserRoute = () => {
+    const location = useLocation()
+
     const { userId } = useContext(UserContext)
+    const { setRedirect } = useContext(RedirectContext)
 
     if (userId === 0) {
         const { authUserId, authLoading } = useAuth()
@@ -15,6 +19,7 @@ const UserRoute = () => {
         }
     
         if (authUserId === 0) {
+            setRedirect(location.pathname)
             return <Navigate to="/login" replace />
         } else if (authUserId !== 0) {
             return
