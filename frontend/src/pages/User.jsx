37c7b8bf26f2a4451d738c1f5599117014e4f04b
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-10-20 20:02:48 -0300

    adds the deAuth user reset

--- frontend/src/pages/User.jsx
+++ frontend/src/pages/User.jsx
@@ -1,22 +1,31 @@
 // Components
 import { Button } from "@/components/ui/button"
+import { ReloadIcon } from "@radix-ui/react-icons"
 
 // Modules
 import dbFetch from "@/config/axios"
 import { UserContext } from "@/context/UserContext"
 import { useContext, useState } from "react"
+import { useNavigate } from "react-router-dom"
 import { toast } from "react-toastify"
 
 const User = () => {
+    const navigate = useNavigate()
     const [loading, setLoading] = useState(false)
 
-    const { setUserId } = useContext(UserContext)
+    const { setUserId, setUserPages } = useContext(UserContext)
 
     const deAuth = async() => {
         setLoading(true)
 
         try {
-            console.log("Logged out")
+            const res = await dbFetch.get("/users/deauth")
+
+            setUserId(0)
+            setUserPages([])
+
+            toast.success(res.data.msg)
+            navigate("/")
         } catch (error) {
             toast.error(error.response.data.msg)
             setLoading(false)
@@ -27,9 +36,19 @@ const User = () => {
         <div className="flex flex-col justify-center items-center gap-12 bg-bgcolor min-h-96">
             <h2 className="text-textcolor font-bold text-2xl">Minha conta</h2>
 
-            <Button>Mudar Informações</Button>
-            <Button variant="destructive">Deletar Conta</Button>
-            <Button onClick={() => deAuth()}>Sair</Button>
+            {!loading ? (
+                <>
+                    <Button>Mudar Informações</Button>
+                    <Button variant="destructive">Deletar Conta</Button>
+                    <Button onClick={() => deAuth()}>Sair</Button>
+                </>
+            ) : (
+                <>
+                    <Button disabled><ReloadIcon className="mr-2 h-4 w-4 animate-spin" />Carregando</Button>
+                    <Button variant="destructive" disabled><ReloadIcon className="mr-2 h-4 w-4 animate-spin" />Carregando</Button>
+                    <Button disabled><ReloadIcon className="mr-2 h-4 w-4 animate-spin" />Carregando</Button>
+                </>
+            )}
         </div>
     )
 }
