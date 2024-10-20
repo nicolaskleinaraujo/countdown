73454a5420fce7818ceca8f2e390414243385e11
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-10-20 18:03:37 -0300

    adds basic structure to user page

--- frontend/src/pages/User.jsx
+++ frontend/src/pages/User.jsx
@@ -1,7 +1,14 @@
+// Components
+import { Button } from "@/components/ui/button"
+
+// Modules
+
 const User = () => {
     return (
-        <div>
-            User
+        <div className="flex flex-col justify-center items-center gap-10 bg-bgcolor min-h-96">
+            <Button>Mudar Informações</Button>
+            <Button variant="destructive">Deletar Conta</Button>
+            <Button>Sair</Button>
         </div>
     )
 }
