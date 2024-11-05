bff452e2497be08b042b2e5ee8e74ce6201d3006
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-11-05 09:55:05 -0300

    adds responsivity to the trigger button

--- frontend/src/components/NewPage.jsx
+++ frontend/src/components/NewPage.jsx
@@ -61,7 +61,7 @@ const NewPage = () => {
                 {/* TODO add zod form */}
                 <DialogTrigger asChild>
                     { userPages.length !== 0 ? (
-                        <Button>Nova <span className="animate-pulse text-purple-400 ml-1">Page</span></Button>
+                        <Button className="md:text-lg md:p-5 md:my-3">Nova <span className="animate-pulse text-purple-400 ml-1">Page</span></Button>
                     ) : (
                         <Button>Crie sua <span className="animate-pulse text-purple-400 ml-1">Page</span></Button>
                     )}
