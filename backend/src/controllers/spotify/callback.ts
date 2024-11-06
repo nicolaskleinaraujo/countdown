48ba8275d0d1942e01cef4c75a62412b1eb425f7
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-11-06 19:23:10 -0300

    creates the callback controller

---
+++ backend/src/controllers/spotify/callback.ts
@@ -0,0 +1,37 @@
+import { Request, Response } from "express"
+import z from "zod"
+import spotifyToken from "../../config/spotifyToken"
+
+interface Info {
+    code: string | null
+}
+
+const InfoSchema = z.object({
+    code: z.string().trim().max(200),
+})
+
+export const callback = async(req: Request, res: Response): Promise<void> => {
+    try {
+        const { code }: Info = InfoSchema.parse(req.query)
+
+        const response = await spotifyToken(code)
+
+        res.cookie("spotifyAccess", response.access_token, {
+            httpOnly: true,
+            signed: true,
+            secure: true,
+            sameSite: "none",
+        })
+
+        res.cookie("spotifyRefresh", response.refresh_token, {
+            httpOnly: true,
+            signed: true,
+            secure: true,
+            sameSite: "none",
+        })
+
+        res.status(200).json({ msg: "Spotify sincronizado" })
+    } catch (error) {
+        res.status(500).json({ msg: "Erro interno, tente novamente", error })
+    }
+}
