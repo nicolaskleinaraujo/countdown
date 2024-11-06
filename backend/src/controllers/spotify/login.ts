ef18be6f29383a40605bd7cd88af6d41eeea85dd
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-11-06 18:18:47 -0300

    hotfixes the redirect_uri field on spotify payload

--- backend/src/controllers/spotify/login.ts
+++ backend/src/controllers/spotify/login.ts
@@ -5,7 +5,7 @@ export const login = async(req: Request, res: Response) => {
     const query = querystring.stringify({
         response_type: "code",
         client_id: process.env.SPOTIFY_CLIENT_ID,
-        redirect_uri: "http://localhost:5173",
+        redirect_uri: `${process.env.ORIGIN_URL}/spotify/callback`,
     })
 
     res.status(202).json({ spotifyUri: `https://accounts.spotify.com/authorize?${query}` })
