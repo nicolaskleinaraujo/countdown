30dc5500f83a119f14af3c778c966e4a8f95b088
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-11-09 10:53:28 -0300

    creates the basic structure on refreshToken controller

--- backend/src/controllers/spotify/refreshToken.ts
+++ backend/src/controllers/spotify/refreshToken.ts
@@ -1,5 +1,19 @@
 import { Request, Response } from "express"
+import spotifyRefreshToken from "../../config/spotifyRefreshToken"
 
 export const refreshToken = async(req: Request, res: Response): Promise<void> => {
+    try {
+        const { spotifyRefresh } = req.signedCookies
 
+        if (spotifyRefresh === undefined) {
+            res.status(401).json({ msg: "Você não tem os tokens do Spotify" })
+            return
+        }
+
+        const response = await spotifyRefreshToken(spotifyRefresh)
+
+        console.log(response)
+    } catch (error) {
+        res.status(500).json({ msg: "Erro interno, tente novamente", error })
+    }
 }
