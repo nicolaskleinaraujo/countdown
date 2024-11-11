62dd1b2ac956c1debfee84d42d5bffffb91abc80
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-11-11 10:00:52 -0300

    hotfixes the page image and events return

--- backend/src/controllers/pages/updateMusic.ts
+++ backend/src/controllers/pages/updateMusic.ts
@@ -1,6 +1,17 @@
 import { Request, Response } from "express"
 import { z } from "zod"
 import { prisma } from "../../db/client"
+import { Pages } from "@prisma/client"
+
+interface ImagesInfos {
+    id: number
+    filename: string
+    content: string | Buffer
+}
+
+interface PagesInfos extends Pages {
+    image: ImagesInfos,
+}
 
 interface Infos {
     musicId: string,
@@ -22,10 +33,13 @@ export const updateMusic = async(req: Request, res: Response): Promise<void> => 
             return
         }
 
-        const updatedPage = await prisma.pages.update({
+        const updatedPage: PagesInfos = await prisma.pages.update({
             where: { id },
             data: { musicId: musicId }, 
+            include: { Events: true, image: true },
         })
+
+        updatedPage.image.content = updatedPage.image.content.toString("base64")
 
         res.status(200).json({ msg: "Musica alterada", updatedPage })
     } catch (error) {
