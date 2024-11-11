8aebc066416d7472f34a5c2e0648f67a5e6915ae
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-11-11 09:40:52 -0300

    finalizes the updateMusic controller

--- backend/src/controllers/pages/updateMusic.ts
+++ backend/src/controllers/pages/updateMusic.ts
@@ -1,5 +1,34 @@
 import { Request, Response } from "express"
+import { z } from "zod"
+import { prisma } from "../../db/client"
+
+interface Infos {
+    musicId: string,
+    id: number
+}
+
+const InfosSchema = z.object({
+    musicId: z.string().trim().max(150),
+    id: z.number()
+})
 
 const updateMusic = async(req: Request, res: Response): Promise<void> => {
+    try {
+        const { musicId, id }: Infos = InfosSchema.parse(req.body)
 
+        const page = await prisma.pages.findUnique({ where: { id } })
+        if (!page) {
+            res.status(404).json({ msg: "Página não encontrada" })
+            return
+        }
+
+        const updatedPage = await prisma.pages.update({
+            where: { id },
+            data: { musicId: musicId }, 
+        })
+
+        res.status(200).json({ msg: "Musica alterada", updatedPage })
+    } catch (error) {
+        res.status(500).json({ msg: "Erro interno, tente novamente", error })
+    }
 }
